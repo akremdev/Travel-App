95613a22183c32bc5bb36aa5d4762dda53983932
akremdev <akremdev@gmail.com>
2021-04-26 17:51:05 +0100

    add/get pins restful api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,21 @@
 const express = require ("express");
 const mongoose = require("mongoose");
 const app = express();
+const dotenv = require("dotenv")
+const pinRoute = require("./routes/pins")
+
+dotenv.config()
+
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}).then(() => {
+    console.log("MongoDB Connected");
+}).catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
 
 app.listen(8800, () => {
     console.log("Backend Server is Running!");
