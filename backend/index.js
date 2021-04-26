412e1865029bdaf1bb18f48b08b05e22d9d28eec
akremdev <akremdev@gmail.com>
2021-04-26 18:11:37 +0100

    login + register system

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv")
 const pinRoute = require("./routes/pins")
+const userRoute = require("./routes/users")
 
 dotenv.config()
 
@@ -10,12 +11,14 @@ app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true, 
+    useCreateIndex :  true
 }).then(() => {
     console.log("MongoDB Connected");
 }).catch((err) => console.log(err));
 
 app.use("/api/pins", pinRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
     console.log("Backend Server is Running!");
