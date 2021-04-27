271f6f3abde17e94bc2828e95e79c422944b497f
akremdev <akremdev@gmail.com>
2021-04-27 02:12:11 +0100

    configure map-gl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import ReactMapGL, {Marker} from 'react-map-gl';
+import {Room} from "@material-ui/icons";
 
 
 function App() {
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    latitude: 47,
-    longitude: 16,
-    zoom: 8
+    latitude: 46,
+    longitude: 17,
+    zoom: 4
   });
 
   return (
@@ -17,10 +18,11 @@ function App() {
       {...viewport}
       mapboxApiAccessToken= {process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
+      mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
       
   >
      <Marker latitude={48.858093} longitude={2.294694} offsetLeft={-20} offsetTop={-10}>
-        
+        <Room style= {{fontSize: viewport.zoom * 7, color:"slateblue"}} />
       </Marker>
   </ReactMapGL>
     </div>
