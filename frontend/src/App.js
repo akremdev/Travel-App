a14fd7f84eeb0273e1e483a1a41ed4e8989d51ee
akremdev <akremdev@gmail.com>
2021-04-27 17:02:23 +0100

    React fetching data from nodejs using axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import {Room, Star} from "@material-ui/icons";
 import './app.css'
+import axios from 'axios';
+import {format } from 'timeago.js'
 
 function App() {
+  const currentUser = "Akrem Gharbi"
+  const [pins, setPins] = useState([])
+  const [currentPlaceId, setCurrentPlaceId] = useState(null)
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -11,6 +16,22 @@ function App() {
     longitude: 17,
     zoom: 4
   });
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res  = await axios.get("/pins")
+        setPins(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getPins();
+  }, [])
+
+  const handleMarkerClick = (id) => {
+    setCurrentPlaceId(id);
+  }
 
   return (
     <div className="App">
@@ -21,20 +42,29 @@ function App() {
       mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
       
   >
-     <Marker latitude={48.858093} longitude={2.294694} offsetLeft={-20} offsetTop={-10}>
-        <Room style= {{fontSize: viewport.zoom * 7, color:"slateblue"}} />
+    {pins.map(p => ( 
+      
+    <> 
+     <Marker latitude={p.lat} longitude={p.long} offsetLeft={-20} offsetTop={-10}>
+        <Room 
+        style= {{fontSize: viewport.zoom * 7,  color: p.username === currentUser ? "tomato" : "slateblue", cursor: "pointer"}} 
+        onClick={() =>handleMarkerClick(p._id)}
+        />
       </Marker>
-      <Popup
-          latitude={48.858093}
-          longitude={2.294694}
+      {p._id === currentPlaceId && (
+        <Popup
+          latitude={p.lat}
+          longitude={p.long}
           closeButton={true}
           closeOnClick={false}
-          anchor="left" >
+          anchor="left" 
+          onClose={()=>setCurrentPlaceId(null)}
+          >
           <div className="card">
             <label>Place</label>
-            <h4 className="place">Eiffel Tower</h4>
+            <h4 className="place">{p.title}</h4>
             <label>Review</label>
-            <p className="desc">Beautiful place. I like it.</p>
+            <p className="desc">{p.desc}</p>
             <label>Rating</label>
             <div className="stars">
               <Star className="star" />
@@ -44,10 +74,14 @@ function App() {
               <Star className="star" />
             </div>
             <label>Information</label>
-            <span className="username">Created by <b>akrem</b></span>
-            <span className="date">1 hour ago </span>
+            <span className="username">Created by <b>{p.username}</b></span>
+            <span className="date">{format(p.createdAt)} </span>
           </div>
-        </Popup>
+        </Popup> 
+      )
+    }
+        </>
+        ))}
   </ReactMapGL>
     </div>
   );
